b81e95154db4c2c3b2b09a1b75e062eee182fd3a
Aiko Vandeputte <aiko@aikovdp.me>
2024-06-25 20:05:51 +0200

    Fix base32 implementation

--- test/lib/base32.test.ts
+++ test/lib/base32.test.ts
@@ -1,6 +1,8 @@
 import { expect, test } from "vitest";
 import * as base32 from "../../src/lib/base32";
 
+const decoder = new TextDecoder();
+
 test.each([
   ["MY======", "f"],
   ["MZXQ====", "fo"],
@@ -8,8 +10,15 @@ test.each([
   ["MZXW6YQ=", "foob"],
   ["MZXW6YTB", "fooba"],
   ["MZXW6YTBOI======", "foobar"],
-])("%s decodes to %s", (encoded, expectedString) => {
-  const expected = new TextEncoder().encode(expectedString).buffer
+  ["K5QWCYLB", "Waaaa"]
+])("%s decodes to %s", (encoded, expected) => {
 
-  expect(base32.decode(encoded)).toEqual(expected)
+  expect(decoder.decode(base32.decode(encoded))).toStrictEqual(expected)
+})
+
+test.each([
+  ["MZXQ====", "12345"]
+])("%s should not decode to %s", (encoded, expectedString) => {
+  const expected = new TextEncoder().encode(expectedString).buffer
+  expect(base32.decode(encoded)).not.toStrictEqual(expected)
 })
